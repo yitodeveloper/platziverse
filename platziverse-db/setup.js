a07a99b94130f66a9c556e79f0246760b695a30f
Rodrigo Ramírez <yitodeveloper@gmail.com>
2019-04-08 13:42:18 -0400

    Crear estructura y la reeemplaza en la base de datos

---
+++ platziverse-db/setup.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const debug = require('debug')('platziverse:db:setup')
+const db = require('./');
+const chalk = require('chalk');
+const inquirer = require('inquirer');
+
+const propmt = inquirer.createPromptModule();
+
+async function setup () {
+  const answer = await propmt([
+    {
+      type: 'confirm',
+      name: 'setup',
+      message: 'This will be destroy your database, are you sure?'
+    }
+  ])
+
+  if(!answer.setup) {
+      return console.log('Nothing happened :)')
+  }
+
+  const config = {
+    database: process.env.DB_NAME || 'platziverse',
+    username: process.env.DB_USER || 'platzi',
+    password: process.env.DB_PASS || 'platzi',
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'postgres',
+    logging: s => debug(s),
+    setup: true
+  }
+
+  await db(config).catch(handleFatalError)
+
+  console.log('success')
+  process.exit(0)
+}
+
+function handleFatalError (err) {
+  console.error(`${chalk.red("[Fatal Error]")} ${err.message}`)
+  console.error(err.stack)
+  process.exit(1)
+}
+setup()
